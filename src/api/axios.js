02f7a8b7f541e824701506ea64272206329a6df8
Vikas24Chauhan <vikas24chauhan@gmail.com>
2026-09-02 15:59:42 +0530

    frontend url added

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,31 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:5000",
+// });
+
+// // Add token to every request
+// api.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("token");
+
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+
+//     return config;
+//   },
+//   (error) => Promise.reject(error),
+// );
+
+// export default api;
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://node-mysql-661s.onrender.com",
 });
 
-// Add token to every request
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
